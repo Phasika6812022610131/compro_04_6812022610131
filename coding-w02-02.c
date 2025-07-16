55d841882df6412467a46996588c62b9256fb7a8
Phasika <s6812022610131@email.kmutnb.ac.th>
2025-07-16 16:45:22 +0700

    WORK

---
+++ coding-w02-02.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // เรียกฟังก์ชัน printf() จาก standard I/O library
+
+int main() {
+    int a = 10; // int : Keyword, a : identifier, = : Operator, 10 : Constant
+    printf("Value of a: %d\n", a); // printf : identifier, "Value of a: %d\n" : Format string, %d : Format specifier, a : Variable
+    return 0; // return : Keyword, 0 : Constant 
+}
---
+++ coding-w04-01.c
@@ -0,0 +1,5 @@
+#include <stdio.h> // เรียกฟังก์ชัน printf() จาก standard I/O library
+int main() { // int : Keyword, main : identifier, () : Operator
+    printf("Hello,\nC Tokens!\n"); // printf : identifier, "Hello" : Constant, \n : newline, ';' : Operator
+    return 0; // return : Keyword, 0 : Constant
+}
