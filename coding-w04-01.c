e3fc72b4a68cb6da3e22b249b0283b02b966ad38
Phasika <XYZ@example.com>
2025-07-18 12:27:06 +0700

    compro-w04

--- coding-w04-01.c
+++ coding-w04-01.c
@@ -1,5 +1,10 @@
 #include <stdio.h> // เรียกฟังก์ชัน printf() จาก standard I/O library
 int main() { // int : Keyword, main : identifier, () : Operator
     printf("Hello,\nC Tokens!\n"); // printf : identifier, "Hello" : Constant, \n : newline, ';' : Operator
-    return 0; // return : Keyword, 0 : Constant
+    return 0; // return : Keyword, 0 : Constant ,  ; : Operator 
 }
+    // Token Type อยู่ในโค้ด
+    // Keyword: int, return
+    // Identifier: main, printf
+    // Operator: (), ;, =
+    // Constant: "Hello", "C Tokens!", 0
